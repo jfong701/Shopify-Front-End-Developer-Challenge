bb569218aa653a5795a10a278ba7c93ac9cf01f1
Jason Fong <jasonc.fong@mail.utoronto.ca>
2018-01-10 23:22:50 -0500

    re-added js file that was missed in last commit

--- shopify-fed-challenge.js
+++ shopify-fed-challenge.js
@@ -1,3 +1,6 @@
+/** 
+Executed when button "Sign up now" button is clicked
+*/
 function signUp() {
 	var msg_prompt = document.getElementById("message");
 	var email_elem = document.getElementById("email");
@@ -12,16 +15,19 @@ function signUp() {
 	}
 }
 
-/*
-Simple JS email validation from: https://stackoverflow.com/questions/46155/how-can-you-validate-an-email-address-in-javascript
+/**
+Helper function for email validation.
+Simple JS email validation from: 
+https://stackoverflow.com/questions/46155/how-can-you-validate-an-email-address-in-javascript
 */
 function validEmail(email) {
 	var regex = /\S+@\S+\.\S+/;
 	return regex.test(email);
 }
 
-
-/* returns a string of what would be sent to the server. */
+/**
+Helper function that returns a string of what would be sent to the server.
+*/
 function getSelectedInterest() {
 	var interests_elem = document.getElementById("interests");
 	return interests_elem.options[interests_elem.selectedIndex].value;
