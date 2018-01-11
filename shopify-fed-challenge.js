3379ed1555293ddf2317a68bff2239eddd858eb1
Jason Fong <jasonc.fong@mail.utoronto.ca>
2018-01-10 22:32:48 -0500

    script now extracts value of email textfield and alerts it

--- shopify-fed-challenge.js
+++ shopify-fed-challenge.js
@@ -1,5 +1,9 @@
 function signUp() {
 	var msg_prompt = document.getElementById("message");
+	var email_elem = document.getElementById("email");
 	
+	var email = email_elem.value;
+	alert(typeof(email));
+	alert(email);
 	msg_prompt.style.opacity = 1;
 }
